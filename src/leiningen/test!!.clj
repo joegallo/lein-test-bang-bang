94f81d8d00caf979f6690a54dc67cb51d15670e5
Zack Maril <zack@zacharymaril.com>
2013-03-11 21:53:41 +0400

    Update for leiningen 2.0. Under my namespace for the moment only to get it on clojars for testing and immediate use.

--- src/leiningen/test!!.clj
+++ src/leiningen/test!!.clj
@@ -1,13 +1,13 @@
 (ns leiningen.test!!
   (:refer-clojure :exclude [test])
-  (:use [leiningen.util.ns :only [namespaces-in-dir]]
-        [leiningen.compile :only [eval-in-project]]
+  (:use [bultitude.core :only [namespaces-in-dir]]
+        [leiningen.core.eval :only [eval-in-project]]
         [leiningen.clean :only [clean]]
         [leiningen.deps :only [deps]]
         [leiningen.test :only [test *exit-after-tests*]]))
 
 (defn test-nses [project]
-  (sort (namespaces-in-dir (:test-path project))))
+  (sort (flatten (map namespaces-in-dir (:test-paths project)))))
 
 (defn test!! [project]
   (doto project clean deps)
