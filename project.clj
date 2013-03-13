d7b23ac4c83e8a60a6b789fa6da58e7c90ff8ffb
Joe Gallo <joegallo@gmail.com>
2013-03-13 11:47:04 -0400

    Add :description and :url

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject lein-test-bang-bang "0.2.0-SNAPSHOT"
   :description "Run your test namespaces in separate JVMs"
+  :url "https://github.com/joegallo/lein-test-bang-bang/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[bultitude "0.2.1" :exclusions [dynapath]]]
   :eval-in-leiningen true)
