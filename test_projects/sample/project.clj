7e14e8e8914dfe28b29c79650f3fce82d9a4de57
Zack Maril <zack@zacharymaril.com>
2013-03-13 19:35:53 +0400

    Update project.clj
    group-id's back to normal

--- test_projects/sample/project.clj
+++ test_projects/sample/project.clj
@@ -1,4 +1,4 @@
 (defproject sample "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]]
-  :plugins [[zmaril/lein-test-bang-bang "0.2.0-SNAPSHOT"]])
+  :plugins [[lein-test-bang-bang "0.2.0-SNAPSHOT"]])
